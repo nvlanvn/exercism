8a271169e8b78baa90b4fa8aec43ac2b2c3e76f0
Lan Nguyen <hey@nvlan.me>
2023-03-12 09:53:20 +0700

    feat/vihicle-purchase: character 6

---
+++ javascript/vehicle-purchase/.history/vehicle-purchase_20230312094759.js
@@ -0,0 +1,56 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines whether or not you need a licence to operate a certain kind of vehicle.
+ *
+ * @param {string} kind
+ * @returns {boolean} whether a license is required
+ */
+
+const 
+
+export function needsLicense(kind) {
+  if (kind == 'car' || kind == 'truck')
+    return true;
+  return false;
+}
+
+/**
+ * Helps choosing between two options by recommending the one that
+ * comes first in dictionary order.
+ *
+ * @param {string} option1
+ * @param {string} option2
+ * @returns {string} a sentence of advice which option to choose
+ */
+export function chooseVehicle(option1, option2) {
+  if(option1 == 'Toyota Corolla' || option2 == 'Toyota Corolla')
+    return 'Toyota Corolla';
+  if(option1 == 'Volkswagen Beetle' || option2 == 'Volkswagen Beetle') 
+    return 'Volkswagen Beetle';
+  return option1;
+}
+
+/**
+ * Calculates an estimate for the price of a used vehicle in the dealership
+ * based on the original price and the age of the vehicle.
+ *
+ * @param {number} originalPrice
+ * @param {number} age
+ * @returns {number} expected resell price in the dealership
+ */
+export function calculateResellPrice(originalPrice, age) {
+  let discount = 0.8;
+  if (age < 3) {
+    discount = 0.8;
+  } else if(3 <= age && age <= 10) {
+    discount = 0.7;
+  } else {
+    discount = 0.5;
+  }
+  return originalPrice * discount;
+}
