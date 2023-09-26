50ebabe33422c9d92423e5ea2e37f096a53012b4
Lan Nguyen <ndolanvn@gmail.com>
2023-09-26 10:08:24 +0700

    feat: implement assembly line

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -3,11 +3,23 @@ class AssemblyLine
     @speed = speed
   end
 
+  def rate
+    if @speed >= 10
+      return 0.77
+    elsif @speed >= 9
+      return 0.8
+    elsif @speed <= 8 and @speed >= 5
+      return 0.9
+    else
+      return 1.0
+    end
+  end
+
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    @speed * 221 * rate
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).to_i
   end
 end
